df1ffb9633d36a2eac9e361d84f04c0b5e9a04b5
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2012-03-16 10:00:03 +1100

    non-copying conversions to Repa-3
    this is a repa array instance of the accelerate array data format, so that repa can read and write (compute in parallel) directly to accelerate arrays.
    
    Conflicts:
    
    	Data/Array/Accelerate/IO.hs

--- Data/Array/Accelerate/IO.hs
+++ Data/Array/Accelerate/IO.hs
@@ -1,33 +1,38 @@
 -- |
 -- Module      : Data.Array.Accelerate.IO
--- Copyright   : [2010..2011] Sean Seefried, Trevor L. McDonell
+-- Copyright   : [2010..2012] Sean Seefried, Trevor L. McDonell
 -- License     : BSD3
 --
 -- Maintainer  : Manuel M T Chakravarty <chak@cse.unsw.edu.au>
 -- Stability   : experimental
 -- Portability : non-portable (GHC extensions)
 --
--- This module provides functions for efficient block copies of primitive arrays
--- (i.e. one dimensional, in row-major order in contiguous memory) to Accelerate
--- Arrays.
+-- This module provides efficient conversion routines between different array
+-- types and Accelerate Arrays.
 --
--- You should only use this module if you really know what you are doing.
--- Potential pitfalls include:
+-- The Repa interface provides an efficient non-copying instance for Repa to
+-- read and write directly into arrays that can then be passed to Accelerate.
+-- Additional copying conversions of low-level primitive arrays (i.e. one
+-- dimensional, row-major blocks of contiguous memory) are provided, however to
+-- use these you should really know what you are doing. Potential pitfalls
+-- include:
 --
 --   * copying from memory your program doesn't have access to (e.g. it may be
---     unallocated or not enough memory is allocated)
+--     unallocated, or not enough memory is allocated)
 --
 --   * memory alignment errors
 --
 
 module Data.Array.Accelerate.IO (
 
-  module Data.Array.Accelerate.IO.Ptr,
   module Data.Array.Accelerate.IO.ByteString,
+  module Data.Array.Accelerate.IO.Ptr,
+  module Data.Array.Accelerate.IO.Repa,
   module Data.Array.Accelerate.IO.Vector
 
 ) where
 
 import Data.Array.Accelerate.IO.Ptr
+import Data.Array.Accelerate.IO.Repa
 import Data.Array.Accelerate.IO.ByteString
 import Data.Array.Accelerate.IO.Vector
---
+++ Data/Array/Accelerate/IO/Repa.hs
@@ -0,0 +1,155 @@
+{-# LANGUAGE ExplicitForAll         #-}
+{-# LANGUAGE EmptyDataDecls         #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE UndecidableInstances   #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE FunctionalDependencies #-}
+-- |
+-- Module      : Data.Array.Accelerate.IO.Repa
+-- Copyright   : [2012] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Manuel M T Chakravarty <chak@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.IO.Repa (
+
+  A, R.Array(..), Shapes,
+  fromRepa, toRepa,
+  computeAccS, computeAccP
+
+) where
+
+import Control.Monad
+import Control.Monad.ST.Unsafe
+
+import qualified Data.Array.Repa                        as R
+import qualified Data.Array.Repa.Eval                   as R
+import qualified Data.Array.Accelerate.Array.Data       as A
+import qualified Data.Array.Accelerate.Array.Sugar      as A
+
+
+-- | Index conversion and equivalence statement between Repa and Accelerate
+-- array shapes. That is, a n-dimensional Repa array will produce an
+-- n-dimensional accelerate array of the same extent, and vice-versa.
+--
+class (R.Shape r, A.Shape a) => Shapes r a | a -> r, r -> a where
+  -- these are really equivalent representations, so unsafeCoerce would probably
+  -- work, but bad programmers get no cookies.
+  toR   :: a -> r
+  toA   :: r -> a
+
+instance Shapes R.Z A.Z where
+  {-# INLINE toR #-}
+  toR A.Z = R.Z
+  {-# INLINE toA #-}
+  toA R.Z = A.Z
+
+instance Shapes sr sa => Shapes (sr R.:. Int) (sa A.:. Int) where
+  {-# INLINE toR #-}
+  toR (sa A.:. sz) = toR sa R.:. sz
+  {-# INLINE toA #-}
+  toA (sr R.:. sz) = toA sr A.:. sz
+
+
+-- | An implementation based on `Data.Array.Accelerate` arrays. The Accelerate
+-- array implementation is based on type families and picks an efficient,
+-- unboxed representation for every element type. Moreover, these arrays can be
+-- handed efficiently (without copying) to Accelerate programs for, example,
+-- further parallel computation on the GPU.
+--
+data A
+data instance R.Array A sh e
+  = AAccelerate {-# UNPACK #-} !sh
+                {-# UNPACK #-} !(A.ArrayData (A.EltRepr e))
+
+-- Repr ------------------------------------------------------------------------
+
+-- | Reading elements of the Accelerate array
+--
+instance A.Elt e => R.Repr A e where
+  {-# INLINE extent #-}
+  extent (AAccelerate sh _)
+    = sh
+
+  {-# INLINE linearIndex #-}
+  linearIndex (AAccelerate sh adata) ix
+    | ix >= 0 && ix < R.size sh
+    = A.toElt (adata `A.indexArrayData` ix)
+
+    | otherwise
+    = error "Repa: accelerate array out of bounds"
+
+  {-# INLINE unsafeLinearIndex #-}
+  unsafeLinearIndex (AAccelerate _ adata) ix
+    = A.toElt (adata `A.indexArrayData` ix)
+
+  {-# INLINE deepSeqArray #-}
+  deepSeqArray (AAccelerate sh adata) x
+    = sh `R.deepSeq` adata `seq` x
+
+
+-- | Filling Accelerate arrays
+--
+instance A.Elt e => R.Fillable A e where
+  data MArr A e
+    = forall s. MAArr (A.MutableArrayData s (A.EltRepr e))
+
+  {-# INLINE newMArr #-}
+  newMArr n
+    = MAArr `liftM` unsafeSTToIO (A.newArrayData n)
+
+  {-# INLINE unsafeWriteMArr #-}
+  unsafeWriteMArr (MAArr mad) n e
+    = unsafeSTToIO
+    $ A.writeArrayData mad n (A.fromElt e)
+
+  {-# INLINE unsafeFreezeMArr #-}
+  unsafeFreezeMArr sh (MAArr mad)
+    = do adata  <- unsafeSTToIO $ A.unsafeFreezeArrayData mad
+         return $! AAccelerate sh adata
+
+-- Conversions -----------------------------------------------------------------
+
+-- | /O(1)/. Wrap an Accelerate array.
+--
+toRepa
+    :: Shapes sh sh'
+    => A.Array sh' e -> R.Array A sh e
+{-# INLINE toRepa #-}
+toRepa arr@(A.Array _ adata)
+  = AAccelerate (toR (A.shape arr)) adata
+
+-- | /O(1)/. Unpack to an Accelerate array.
+--
+fromRepa
+    :: (Shapes sh sh', A.Elt e)
+    => R.Array A sh e -> A.Array sh' e
+{-# INLINE fromRepa #-}
+fromRepa (AAccelerate sh adata)
+  = A.Array (A.fromElt (toA sh)) adata
+
+
+-- Computations ----------------------------------------------------------------
+
+-- | Sequential computation of array elements
+--
+computeAccS
+    :: R.Fill r A sh e
+    => R.Array r sh e -> R.Array A sh e
+{-# INLINE computeAccS #-}
+computeAccS = R.computeS
+
+-- | Parallel computation of array elements
+--
+computeAccP
+    :: R.Fill r A sh e
+    => R.Array r sh e -> R.Array A sh e
+{-# INLINE computeAccP #-}
+computeAccP = R.computeP
+
