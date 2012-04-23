24abbd09d8f5dfeff82fa671d81b60c52ff844bb
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2012-04-23 17:07:40 +1000

    Re-add Vector support that got lost during the merge

--- Data/Array/Accelerate/IO.hs
+++ Data/Array/Accelerate/IO.hs
@@ -28,9 +28,11 @@ module Data.Array.Accelerate.IO (
   module Data.Array.Accelerate.IO.ByteString,
   module Data.Array.Accelerate.IO.Ptr,
   module Data.Array.Accelerate.IO.Repa,
+  module Data.Array.Accelerate.IO.Vector,
 
 ) where
 
+import Data.Array.Accelerate.IO.ByteString
 import Data.Array.Accelerate.IO.Ptr
 import Data.Array.Accelerate.IO.Repa
-import Data.Array.Accelerate.IO.ByteString
+import Data.Array.Accelerate.IO.Vector
