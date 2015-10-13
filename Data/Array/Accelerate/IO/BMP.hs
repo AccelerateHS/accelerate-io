81d694c0fdeb4958d7bda57cc1a96db8cb135c5e
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-10-13 15:49:45 -0400

    Revert "fix for changes to EltlRepr"
    This reverts commit b990e23a77b1e6b3e8a32bb3751c3ac90fee6ee1.

--- Data/Array/Accelerate/IO/BMP.hs
+++ Data/Array/Accelerate/IO/BMP.hs
@@ -46,7 +46,7 @@ readImageFromBMP file = do
       let (w,h) = bmpDimensions bmp
           bs    = unpackBMPToRGBA32 bmp
       --
-      Right `fmap` A.fromByteString (Z :. h :. w) bs
+      Right `fmap` A.fromByteString (Z :. h :. w) ((), bs)
 
 
 -- | Write the image data to a file.
@@ -54,7 +54,7 @@ readImageFromBMP file = do
 writeImageToBMP :: FilePath -> Array DIM2 RGBA32 -> IO ()
 writeImageToBMP file rgba = do
   let Z :. h :. w       =  A.arrayShape rgba
-  bs                    <- A.toByteString rgba
+  ((), bs)              <- A.toByteString rgba
   --
   writeBMP file (packRGBA32ToBMP w h bs)
 
