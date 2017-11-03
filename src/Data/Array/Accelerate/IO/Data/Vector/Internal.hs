b6dd4d6dc0fec502c0a49fd9ce8e79d32beb43c4
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-11-03 21:32:45 +1100

    add conversion from primitive vector

---
+++ src/Data/Array/Accelerate/IO/Data/Vector/Internal.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE MagicHash     #-}
+{-# LANGUAGE UnboxedTuples #-}
+-- |
+-- Module      : Data.Array.Accelerate.IO.Data.Vector.Internal
+-- Copyright   : [2017] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.IO.Data.Vector.Internal
+  where
+
+import Data.Primitive.ByteArray
+
+import GHC.Base
+import GHC.ForeignPtr
+
+
+-- Return the ByteArray underlying a ForeignPtr, or a new byte array if it is
+-- not a Plain ForeignPtr.
+--
+byteArrayOfForeignPtr :: Int -> ForeignPtr a -> IO ByteArray
+byteArrayOfForeignPtr (I# bytes#) (ForeignPtr addr# c) = IO $ \s ->
+  case c of
+    PlainPtr mba# -> case unsafeFreezeByteArray# mba# s of
+                       (# s', ba#  #) -> (# s', ByteArray ba# #)
+
+    _             -> case newAlignedPinnedByteArray# bytes# 16# s of
+                       (# s1, mba# #) -> case copyAddrToByteArray# addr# mba# 0# bytes# s1 of
+                                           s2 -> case unsafeFreezeByteArray# mba# s2 of
+                                                   (# s3, ba# #) -> (# s3, ByteArray ba# #)
+
+
+-- Return the ByteArray as a ForeignPtr. This will attempt a non-copying
+-- conversion, if the underlying byte array is pinned.
+--
+foreignPtrOfByteArray :: Int -> Int -> ByteArray -> IO (ForeignPtr a)
+foreignPtrOfByteArray (I# soff#) (I# bytes#) (ByteArray ba#) = IO $ \s ->
+  case isByteArrayPinned# ba# of
+    0# -> case newAlignedPinnedByteArray# bytes# 16# s of
+            (# s1, mba# #) -> case copyByteArray# ba# 0# mba# soff# bytes# s1 of
+                                s2 -> (# s2, ForeignPtr (byteArrayContents# (unsafeCoerce# mba#)) (PlainPtr mba#) #)
+
+    _  -> (# s, ForeignPtr (byteArrayContents# ba#) (PlainPtr (unsafeCoerce# ba#)) #)
+
---
+++ src/Data/Array/Accelerate/IO/Data/Vector/Primitive.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeFamilies        #-}
+-- |
+-- Module      : Data.Array.Accelerate.IO.Data.Vector.Primitive
+-- Copyright   : [2017] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+-- Efficient conversion between 'Data.Vector.Primitive' vectors and Accelerate
+-- 'Array's.
+--
+
+module Data.Array.Accelerate.IO.Data.Vector.Primitive (
+
+  Vectors,
+  toVectors,
+  fromVectors,
+
+) where
+
+import Data.Primitive                                               ( sizeOf )
+import Data.Vector.Primitive
+
+import Data.Array.Accelerate.IO.Data.Vector.Internal
+
+import Data.Array.Accelerate.Array.Data
+import Data.Array.Accelerate.Array.Sugar                            hiding ( Vector )
+import Data.Array.Accelerate.Array.Unique
+import Data.Array.Accelerate.Error
+import Data.Array.Accelerate.Lifetime
+import qualified Data.Array.Accelerate.Array.Representation         as R
+
+import Data.Int
+import Data.Word
+import System.IO.Unsafe
+
+
+-- | A family of types which represent a collection of Primitive Vectors. The
+-- structure of the collection depends on the element type @e@ of the
+-- corresponding Accelerate array.
+--
+type family Vectors e :: *
+
+type instance Vectors ()     = ()
+type instance Vectors Int    = Vector Int
+type instance Vectors Int8   = Vector Int8
+type instance Vectors Int16  = Vector Int16
+type instance Vectors Int32  = Vector Int32
+type instance Vectors Int64  = Vector Int64
+type instance Vectors Word   = Vector Word
+type instance Vectors Word8  = Vector Word8
+type instance Vectors Word16 = Vector Word16
+type instance Vectors Word32 = Vector Word32
+type instance Vectors Word64 = Vector Word64
+type instance Vectors Float  = Vector Float
+type instance Vectors Double = Vector Double
+type instance Vectors Char   = Vector Char
+type instance Vectors (a,b)  = (Vectors a, Vectors b)
+
+
+-- | /O(n)/ (typically). Convert a collection of primitive vectors into an
+-- Accelerate array.
+--
+-- If the underlying vectors are pinned then this can be done without.
+--
+-- @since 1.1.0.0@
+--
+fromVectors :: (Shape sh, Elt e) => sh -> Vectors (EltRepr e) -> Array sh e
+fromVectors sh vecs = Array (fromElt sh) (aux arrayElt vecs)
+  where
+    wrap :: forall a. Prim a => Vector a -> UniqueArray a
+    wrap (Vector o l ba)
+      = $boundsCheck "fromVectors" "shape mismatch" (size sh == l)
+      $ unsafePerformIO $ newUniqueArray =<< foreignPtrOfByteArray o (l * sizeOf (undefined::a)) ba
+
+    aux :: ArrayEltR e -> Vectors e -> ArrayData e
+    aux ArrayEltRunit           _       = AD_Unit
+    aux ArrayEltRint            v       = AD_Int    (wrap v)
+    aux ArrayEltRint8           v       = AD_Int8   (wrap v)
+    aux ArrayEltRint16          v       = AD_Int16  (wrap v)
+    aux ArrayEltRint32          v       = AD_Int32  (wrap v)
+    aux ArrayEltRint64          v       = AD_Int64  (wrap v)
+    aux ArrayEltRword           v       = AD_Word   (wrap v)
+    aux ArrayEltRword8          v       = AD_Word8  (wrap v)
+    aux ArrayEltRword16         v       = AD_Word16 (wrap v)
+    aux ArrayEltRword32         v       = AD_Word32 (wrap v)
+    aux ArrayEltRword64         v       = AD_Word64 (wrap v)
+    aux ArrayEltRchar           v       = AD_Char   (wrap v)
+    aux ArrayEltRfloat          v       = AD_Float  (wrap v)
+    aux ArrayEltRdouble         v       = AD_Double (wrap v)
+    aux (ArrayEltRpair ad1 ad2) (v1,v2) = AD_Pair   (aux ad1 v1) (aux ad2 v2)
+    --
+    aux _ _ = $internalError "fromVectors" "unsupported type"
+
+
+-- | /O(1)/ (typically). Convert an Accelerate array into a collection of
+-- primitive vectors.
+--
+-- If the array data was allocated by Accelerate, this can typically be done
+-- without copying.
+--
+-- @since 1.1.0.0@
+--
+toVectors :: (Shape sh, Elt e) => Array sh e -> Vectors (EltRepr e)
+toVectors (Array sh adata) = aux arrayElt adata
+  where
+    n :: Int
+    n = R.size sh
+
+    wrap :: forall a. Prim a => UniqueArray a -> Vector a
+    wrap ua = Vector 0 n $ unsafePerformIO
+            $ byteArrayOfForeignPtr (n * sizeOf (undefined::a)) (unsafeGetValue (uniqueArrayData ua))
+
+    aux :: ArrayEltR e -> ArrayData e -> Vectors e
+    aux ArrayEltRunit           AD_Unit         = ()
+    aux ArrayEltRint            (AD_Int v)      = wrap v
+    aux ArrayEltRint8           (AD_Int8 v)     = wrap v
+    aux ArrayEltRint16          (AD_Int16 v)    = wrap v
+    aux ArrayEltRint32          (AD_Int32 v)    = wrap v
+    aux ArrayEltRint64          (AD_Int64 v)    = wrap v
+    aux ArrayEltRword           (AD_Word v)     = wrap v
+    aux ArrayEltRword8          (AD_Word8 v)    = wrap v
+    aux ArrayEltRword16         (AD_Word16 v)   = wrap v
+    aux ArrayEltRword32         (AD_Word32 v)   = wrap v
+    aux ArrayEltRword64         (AD_Word64 v)   = wrap v
+    aux ArrayEltRchar           (AD_Char v)     = wrap v
+    aux ArrayEltRfloat          (AD_Float v)    = wrap v
+    aux ArrayEltRdouble         (AD_Double v)   = wrap v
+    aux (ArrayEltRpair ad1 ad2) (AD_Pair v1 v2) = (aux ad1 v1, aux ad2 v2)
+    --
+    aux _ _ = $internalError "toVectors" "unsupported type"
+
