0e82063e61ce3552c1f6420fd14a5ed82588c0b9
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2020-07-15 17:03:59 +0200

    update accelerate-io-cereal

--- accelerate-io-cereal/src/Data/Array/Accelerate/IO/Data/Serialize.hs
+++ accelerate-io-cereal/src/Data/Array/Accelerate/IO/Data/Serialize.hs
@@ -20,15 +20,16 @@
 module Data.Array.Accelerate.IO.Data.Serialize ()
   where
 
-import Data.Primitive.ByteArray
 import Data.Serialize
-import Foreign.C.Types
-import qualified Data.ByteString                          as B
 
-import Data.Array.Accelerate.Analysis.Type
 import Data.Array.Accelerate.Array.Data
-import Data.Array.Accelerate.Array.Sugar
+import Data.Array.Accelerate.Sugar.Array
+import Data.Array.Accelerate.Sugar.Elt
+import Data.Array.Accelerate.Sugar.Shape
+import Data.Array.Accelerate.Representation.Shape
+import Data.Array.Accelerate.Representation.Type
 import Data.Array.Accelerate.Type
+import qualified Data.Array.Accelerate.Representation.Array         as R
 
 import Data.Array.Accelerate.IO.Data.ByteString
 
@@ -36,149 +37,102 @@ import Data.Array.Accelerate.IO.Data.ByteString
 instance (Shape sh, Elt e) => Serialize (Array sh e) where
   {-# INLINE get #-}
   get = do
-    sh <- toElt <$> getTupleType (eltType @sh)
-    fromByteStrings sh <$> getByteStrings (arrayElt @(EltRepr e))
+    sh <- getShapeR (shapeR @sh)
+    bs <- getArrayR (eltR @e)
+    return $ fromByteStrings (toElt sh) bs
+    where
+      getShapeR :: ShapeR t -> Get t
+      getShapeR ShapeRz = return ()
+      getShapeR (ShapeRsnoc shR) = do
+        sz <- get
+        sh <- getShapeR shR
+        return (sh, sz)
+
+      getArrayR :: TypeR a -> Get (ByteStrings a)
+      getArrayR TupRunit           = return ()
+      getArrayR (TupRpair aR1 aR2) = do
+        a1 <- getArrayR aR1
+        a2 <- getArrayR aR2
+        return (a1, a2)
+      getArrayR (TupRsingle aR) = scalar aR
+        where
+          scalar :: ScalarType a -> Get (ByteStrings a)
+          scalar (SingleScalarType t) = single t
+          scalar (VectorScalarType t) = vector t
+
+          vector :: VectorType a -> Get (ByteStrings a)
+          vector (VectorType _ t)
+            | SingleArrayDict <- singleArrayDict t
+            = single t
+
+          single :: SingleType a -> Get (ByteStrings a)
+          single (NumSingleType t) = num t
+
+          num :: NumType a -> Get (ByteStrings a)
+          num (IntegralNumType t) = integral t
+          num (FloatingNumType t) = floating t
+
+          integral :: IntegralType a -> Get (ByteStrings a)
+          integral TypeInt    = get
+          integral TypeInt8   = get
+          integral TypeInt16  = get
+          integral TypeInt32  = get
+          integral TypeInt64  = get
+          integral TypeWord   = get
+          integral TypeWord8  = get
+          integral TypeWord16 = get
+          integral TypeWord32 = get
+          integral TypeWord64 = get
+
+          floating :: FloatingType a -> Get (ByteStrings a)
+          floating TypeHalf   = get
+          floating TypeFloat  = get
+          floating TypeDouble = get
 
   {-# INLINE put #-}
-  put arr = do
-    putTupleType (eltType @sh) (fromElt (shape arr))
-    putByteStrings (arrayElt @(EltRepr e)) (toByteStrings arr)
+  put arr@(Array (R.Array sh _)) = do
+    putShapeR (shapeR @sh) sh
+    putArrayR (eltR @e) (toByteStrings arr)
+    where
+      putShapeR :: ShapeR t -> t -> Put
+      putShapeR ShapeRz          ()       = return ()
+      putShapeR (ShapeRsnoc stR) (st, sz) = put sz >> putShapeR stR st
 
-{-# INLINEABLE putByteStrings #-}
-putByteStrings :: ArrayEltR t -> ByteStrings t -> Put
-putByteStrings ArrayEltRunit           = return
-putByteStrings ArrayEltRint            = put
-putByteStrings ArrayEltRint8           = put
-putByteStrings ArrayEltRint16          = put
-putByteStrings ArrayEltRint32          = put
-putByteStrings ArrayEltRint64          = put
-putByteStrings ArrayEltRword           = put
-putByteStrings ArrayEltRword8          = put
-putByteStrings ArrayEltRword16         = put
-putByteStrings ArrayEltRword32         = put
-putByteStrings ArrayEltRword64         = put
-putByteStrings ArrayEltRhalf           = put
-putByteStrings ArrayEltRfloat          = put
-putByteStrings ArrayEltRdouble         = put
-putByteStrings ArrayEltRbool           = put
-putByteStrings ArrayEltRchar           = put
-putByteStrings (ArrayEltRvec ae)       = putByteStrings ae
-putByteStrings (ArrayEltRpair ae1 ae2) = \(v1,v2) -> putByteStrings ae1 v1 >>
-                                                     putByteStrings ae2 v2
+      putArrayR :: TypeR a -> ByteStrings a -> Put
+      putArrayR TupRunit           ()       = return ()
+      putArrayR (TupRpair aR1 aR2) (a1, a2) = putArrayR aR1 a1 >> putArrayR aR2 a2
+      putArrayR (TupRsingle aR)    a        = scalar aR a
+        where
+          scalar :: ScalarType a -> ByteStrings a -> Put
+          scalar (SingleScalarType t) = single t
+          scalar (VectorScalarType t) = vector t
 
-{-# INLINEABLE getByteStrings #-}
-getByteStrings :: ArrayEltR e -> Get (ByteStrings e)
-getByteStrings ArrayEltRunit           = return ()
-getByteStrings ArrayEltRint            = get
-getByteStrings ArrayEltRint8           = get
-getByteStrings ArrayEltRint16          = get
-getByteStrings ArrayEltRint32          = get
-getByteStrings ArrayEltRint64          = get
-getByteStrings ArrayEltRword           = get
-getByteStrings ArrayEltRword8          = get
-getByteStrings ArrayEltRword16         = get
-getByteStrings ArrayEltRword32         = get
-getByteStrings ArrayEltRword64         = get
-getByteStrings ArrayEltRhalf           = get
-getByteStrings ArrayEltRfloat          = get
-getByteStrings ArrayEltRdouble         = get
-getByteStrings ArrayEltRbool           = get
-getByteStrings ArrayEltRchar           = get
-getByteStrings (ArrayEltRvec ae)       = getByteStrings ae
-getByteStrings (ArrayEltRpair ae1 ae2) = (,) <$> getByteStrings ae1
-                                             <*> getByteStrings ae2
+          vector :: VectorType a -> ByteStrings a -> Put
+          vector (VectorType _ t)
+            | SingleArrayDict <- singleArrayDict t
+            = single t
 
-putTupleType :: TupleType t -> t -> Put
-putTupleType TypeRunit         ()    = return ()
-putTupleType (TypeRpair ta tb) (a,b) = putTupleType ta a >> putTupleType tb b
-putTupleType (TypeRscalar t)   a     = putScalarType t a
+          single :: SingleType a -> ByteStrings a -> Put
+          single (NumSingleType t) = num t
 
-putScalarType :: ScalarType t -> t -> Put
-putScalarType (SingleScalarType t) = putSingleType t
-putScalarType (VectorScalarType t) = putVectorType t
+          num :: NumType a -> ByteStrings a -> Put
+          num (IntegralNumType t) = integral t
+          num (FloatingNumType t) = floating t
 
-putVectorType :: VectorType (Vec n a) -> Vec n a -> Put
-putVectorType (VectorType n t) (Vec ba#) =
-  let ba    = ByteArray ba#
-      bytes = n * sizeOfSingleType t
-      go !i
-        | i >= bytes = return ()
-        | otherwise  = put (indexByteArray ba i :: Word8) >> go (i+1)
-   in
-   go 0
+          integral :: IntegralType a -> ByteStrings a -> Put
+          integral TypeInt    = put
+          integral TypeInt8   = put
+          integral TypeInt16  = put
+          integral TypeInt32  = put
+          integral TypeInt64  = put
+          integral TypeWord   = put
+          integral TypeWord8  = put
+          integral TypeWord16 = put
+          integral TypeWord32 = put
+          integral TypeWord64 = put
 
-putSingleType :: SingleType t -> t -> Put
-putSingleType (NumSingleType    t) = putNumType t
-putSingleType (NonNumSingleType t) = putNonNumType t
-
-putNumType :: NumType t -> t -> Put
-putNumType (IntegralNumType t) = putIntegralType t
-putNumType (FloatingNumType t) = putFloatingType t
-
-putIntegralType :: IntegralType t -> t -> Put
-putIntegralType TypeInt{}    = put
-putIntegralType TypeInt8{}   = put
-putIntegralType TypeInt16{}  = put
-putIntegralType TypeInt32{}  = put
-putIntegralType TypeInt64{}  = put
-putIntegralType TypeWord{}   = put
-putIntegralType TypeWord8{}  = put
-putIntegralType TypeWord16{} = put
-putIntegralType TypeWord32{} = put
-putIntegralType TypeWord64{} = put
-
-putFloatingType :: FloatingType t -> t -> Put
-putFloatingType TypeHalf{}   = \(Half (CUShort w)) -> put w
-putFloatingType TypeFloat{}  = put
-putFloatingType TypeDouble{} = put
-
-putNonNumType :: NonNumType t -> t -> Put
-putNonNumType TypeBool{} = put
-putNonNumType TypeChar{} = put
-
-getTupleType :: TupleType t -> Get t
-getTupleType TypeRunit         = return ()
-getTupleType (TypeRpair ta tb) = (,) <$> getTupleType ta <*> getTupleType tb
-getTupleType (TypeRscalar t)   = getScalarType t
-
-getScalarType :: ScalarType t -> Get t
-getScalarType (SingleScalarType t) = getSingleType t
-getScalarType (VectorScalarType t) = getVectorType t
-
--- TLM: this could be more efficient 2019-07-05
-getVectorType :: VectorType (Vec n a) -> Get (Vec n a)
-getVectorType (VectorType n t) = do
-  let m = n * sizeOfSingleType t
-  bytes  <- getBytes m
-  return $! case byteArrayFromListN m (B.unpack bytes) of
-              ByteArray ba# -> Vec ba#
-
-getSingleType :: SingleType t -> Get t
-getSingleType (NumSingleType    t) = getNumType t
-getSingleType (NonNumSingleType t) = getNonNumType t
-
-getNumType :: NumType t -> Get t
-getNumType (IntegralNumType t) = getIntegralType t
-getNumType (FloatingNumType t) = getFloatingType t
-
-getIntegralType :: IntegralType t -> Get t
-getIntegralType TypeInt{}    = get
-getIntegralType TypeInt8{}   = get
-getIntegralType TypeInt16{}  = get
-getIntegralType TypeInt32{}  = get
-getIntegralType TypeInt64{}  = get
-getIntegralType TypeWord{}   = get
-getIntegralType TypeWord8{}  = get
-getIntegralType TypeWord16{} = get
-getIntegralType TypeWord32{} = get
-getIntegralType TypeWord64{} = get
-
-getFloatingType :: FloatingType t -> Get t
-getFloatingType TypeHalf{}   = Half . CUShort <$> get
-getFloatingType TypeFloat{}  = get
-getFloatingType TypeDouble{} = get
-
-getNonNumType :: NonNumType t -> Get t
-getNonNumType TypeBool{} = get
-getNonNumType TypeChar{} = get
+          floating :: FloatingType a -> ByteStrings a -> Put
+          floating TypeHalf   = put
+          floating TypeFloat  = put
+          floating TypeDouble = put
 
